ef5538dd949151293d735125a674be27ce774394
Adiitya2328 <adityachoudhary7898@gmail.com>
2026-02-18 21:55:37 +0530

    solved this using a while loop until n%2 == 0

---
+++ PowerOfTwo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n%2 == 0) {
+            n = n/2;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean result = isPowerOfTwo(n);
+
+        if (result) {
+            System.out.println(n + " is a Power of Two.");
+        } else {
+            System.out.println(n + " is not a Power of Two.");
+        }
+
+        
+    }
+}

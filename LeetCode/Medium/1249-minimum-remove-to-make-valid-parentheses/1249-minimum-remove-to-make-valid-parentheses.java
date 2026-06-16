17ecad3609fbaff4da735801877b3d5377e8eb87
Aditya Raj <adityachoudhary7898@gmail.com>
2026-06-16 11:07:28 +0530

    Time: 572 ms (5.09%), Space: 49.2 MB (5.09%) - LeetHub

---
+++ LeetCode/Medium/1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Integer> st = new Stack<>();
+        for(int i =0 ; i< arr.length; i++){
+            if(arr[i] == '('){
+                st.push(i);
+            }else if(arr[i] == ')'){
+                if(st.isEmpty() ){
+                    arr[i] = '!';
+                }else{
+                    st.pop();
+                }
+            }
+        }
+        while(!st.isEmpty()){
+            arr[st.pop()] = '!';
+        }
+
+        String res = "";
+        for(int i = 0 ; i<arr.length ; i++){
+            if(arr[i]!= '!'){
+                res+=arr[i];
+            }
+        }
+        return res;
+    }
+}

321256ab9dcbcb5dd9d4cd9d172a324927073af2
Aditya Raj <adityachoudhary7898@gmail.com>
2026-06-17 16:16:47 +0530

    Time: 3 ms (100%), Space: 55.2 MB (65.93%) - LeetHub

--- LeetCode/Medium/3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ LeetCode/Medium/3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,58 +1,28 @@
 class Solution {
-
-    public String REV(String str) {
-        char[] arr = str.toCharArray();
-
-        int l = 0;
-        int r = arr.length - 1;
-
-        while (l < r) {
-            char temp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = temp;
-
-            l++;
-            r--;
-        }
-
-        return new String(arr);
-    }
-
-    public String REM(String st) {
-        int n = st.length();
-
-        if (n == 0) {
-            return "";
-        }
-
-        String res = "";
-
-        for (int i = 0; i < n - 1; i++) {
-            res += st.charAt(i);
-        }
-
-        return res;
-    }
-
     public String processStr(String s) {
-        String res = "";
+
+        StringBuilder res = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
 
-            if (s.charAt(i) == '#') {
-                res = res + res;
+            char ch = s.charAt(i);
+
+            if (ch == '#') {
+                res.append(res.toString());
             }
-            else if (s.charAt(i) == '%') {
-                res = REV(res);
+            else if (ch == '%') {
+                res.reverse();
             }
-            else if (s.charAt(i) == '*') {
-                res = REM(res);
+            else if (ch == '*') {
+                if (res.length() > 0) {
+                    res.deleteCharAt(res.length() - 1);
+                }
             }
             else {
-                res += s.charAt(i);
+                res.append(ch);
             }
         }
 
-        return res;
+        return res.toString();
     }
 }

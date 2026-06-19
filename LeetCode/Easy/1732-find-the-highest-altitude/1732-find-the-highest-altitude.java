c52e09a8002393dc9ded87a88bb80411cb82c927
Aditya Raj <adityachoudhary7898@gmail.com>
2026-06-19 15:58:18 +0530

    Time: 0 ms (100%), Space: 43.4 MB (6.14%) - LeetHub

--- LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ LeetCode/Easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -2,12 +2,15 @@ class Solution {
     public int largestAltitude(int[] gain) {
         int n = gain.length;
         int res = Integer.MIN_VALUE;
-        Deque<Integer> st = new ArrayDeque<>();
-        st.push(0);
-        for(int i = 0 ; i<n; i++){
-            res = Math.max(st.peek() , res);
-            st.push(st.peek()+gain[i]);
+        int[] prefixSum = new int[n+1];
+        prefixSum[0] = 0;
+        for(int i = 1; i<n+1;i++){
+            prefixSum[i] = prefixSum[i-1]+gain[i-1];
+
         }
-        return Math.max(st.peek(), res);
+        for(int i = 0; i<prefixSum.length;i++){
+            res = Math.max(res, prefixSum[i]);
+        }
+        return res;
     }
 }

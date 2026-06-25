775842da1f0de92091d41cc3b3dc579bb264a35c
Aditya Raj <adityachoudhary7898@gmail.com>
2026-06-26 00:09:00 +0530

    Time: 61 ms (69.84%), Space: 107.9 MB (14.08%) - LeetHub

---
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0 ; i<n;i++){
+//check until the stack is not empty and the temperature is greater than next temp
+            while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()]){
+                int prevIndex = st.pop();//pop the previous index
+
+                res[prevIndex] = i - prevIndex; // calculate the gap between the temps
+
+            }
+            st.push(i); //push the next index of the temperature
+        }
+
+        return res;
+    }
+}

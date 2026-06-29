a5f6a2441d6ad4e7ec88f40ab4422dea9bfb96f4
Aditya Raj <adityachoudhary7898@gmail.com>
2026-06-29 19:20:41 +0530

    Time: 1 ms (75.75%), Space: 43.6 MB (19.94%) - LeetHub

--- LeetCode/Easy/1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ LeetCode/Easy/1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -2,8 +2,8 @@ class Solution {
     public int numOfStrings(String[] patterns, String word) {
         int count = 0;
         for(int i = 0 ; i<patterns.length; i++){
-            String words = patterns[i];
-            boolean exist = word.contains(words);
+           
+            boolean exist = word.contains(patterns[i]);
             if(exist){
                  count++;
             }

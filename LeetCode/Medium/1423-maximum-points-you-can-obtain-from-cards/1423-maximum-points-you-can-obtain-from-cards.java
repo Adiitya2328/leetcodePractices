b4a4ec7c590faa9da26ee484d9707d4d2731244e
Aditya Raj <adityachoudhary7898@gmail.com>
2026-07-03 00:17:18 +0530

    Time: 2 ms (99.75%), Space: 61.5 MB (91.67%) - LeetHub

--- LeetCode/Medium/1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ LeetCode/Medium/1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -2,26 +2,23 @@ class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
         int low = 0;
-        int high  =n-k;//the window size is n-k because we have to take that window which have smallest value
+        int high = n-k;
         int max = Integer.MIN_VALUE;
         int total = 0;
         int sum = 0;
-        for(int i = 0 ; i<n ; i++){
-            total+= cardPoints[i];//calculate the total points of card
+        for(int i = 0 ; i<n; i++){//calculate the total points
+            total += cardPoints[i]; 
         }
-        
-        for(int i = low; i<high ; i++){
-                sum += cardPoints[i];
-            }
-             max = total-sum;
-        while (high < n){
-            
-            sum = sum -cardPoints[low]+cardPoints[high];
-            
-           //reduce the window sum which is smallest from total to get the max value and with max function find out the largest sum
+        for(int i = low ; i< high; i++){
+            sum += cardPoints[i];//calculate the total points of window 
+        }
+        max = total - sum; //initialize the sum
+        while(high< n ){
+            sum = sum - cardPoints[low]+cardPoints[high];
+            //decrement the point at low and increment the point at high
+            max = Math.max(max , total - sum);//recalculate the new max;
             low++;
             high++;
-            max = Math.max(max,total- sum);
         }
         return max;
     }

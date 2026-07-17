e592357041917a07755551d815cd425ee156e0f7
Aditya Raj <adityachoudhary7898@gmail.com>
2026-07-17 22:39:26 +0530

    Time: 16 ms (87.13%), Space: 47 MB (86.38%) - LeetHub

--- LeetCode/Medium/1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ LeetCode/Medium/1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -3,7 +3,7 @@ class Pair {
     int count;
     Pair(char ch , int count){
         this.ch =  ch;
-        this.count= count;
+        this.count= count; //made a pair class to add the pair of character and their count 
     }
 }
 class Solution {
@@ -12,24 +12,24 @@ class Solution {
         int n = s.length();
         for(int i =0;i<n;i++){
             char c = s.charAt(i);
-            if(st.isEmpty()){
+            if(st.isEmpty()){//checked if the st is empty then push the pair 
                 st.push(new Pair(c ,1));
-            }else if(st.peek().ch == c){
+            }else if(st.peek().ch == c){//if ch is same then increase the count
                 st.peek().count++;
-                if(st.peek().count == k){
+                if(st.peek().count == k){// if count matches k during increment delete the top pair 
                     st.pop();
                 }
             }else{
-                st.push(new Pair(c ,1));
+                st.push(new Pair(c ,1));//else add the pair
             }
         }
         StringBuilder res = new StringBuilder();
         while(!st.isEmpty()){
             Pair p = st.pop();
             for(int i=0;i<p.count;i++){
-                res.append(p.ch);
+                res.append(p.ch);//take out the characters only from the pairs inside st
             }
         }
-        return res.reverse().toString();
+        return res.reverse().toString();//reverse the output 
     }
 }

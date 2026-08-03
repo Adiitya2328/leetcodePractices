8f9e8f6ab80b7c8ebbec0f9dd82c8a3584d88893
Aditya Raj <adityachoudhary7898@gmail.com>
2026-08-04 01:45:36 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.39%) - LeetHub

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,33 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        int n = nums.length;
-        int low=0;
-        int high=n-1;
-        int LB =-1;
-        int UB = -1;
+    public int FirstO(int[] arr,  int x){//created a different function to return the first index of the element 
+        int low =0;
+        int high = arr.length-1;
+        int first =-1;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>=target){
-                if(nums[mid] == target)LB= mid;
-                high = mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        low=0;
-        high=n-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]<=target){
-                if(nums[mid]==target)UB = mid;
+            int mid = (low+high)/2;
+            if(arr[mid]==x){
+                first = mid;
+                high =mid-1;
+            }else if(arr[mid]<x){
                 low=mid+1;
             }else{
                 high=mid-1;
             }
+            
         }
-        ans[0] = LB;
-        ans[1] = UB;
-        return ans; 
+        return first;
+    }//created the second function to find the last occurence of the element 
+    public int LastO(int[] arr,  int x){
+        int low =0;
+        int high = arr.length-1;
+        int last =-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==x){
+                last = mid;
+                low =mid+1;
+            }else if(arr[mid]<x){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+            
+        }
+        return last;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int first = FirstO(nums , target);
+        if(first ==-1){//made this condition only to reduce complexity when there will be no first element it should not check for the occurence of the last element directly return [-1,-1]
+            return new int[]{-1,-1};
+        }
+        int last = LastO(nums, target);
+        return new int[]{first,last};
     }
 }

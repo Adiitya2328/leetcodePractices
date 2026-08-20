ddc0daa74cf18d590254f1b6c65c84b078ca99ff
Aditya Raj <adityachoudhary7898@gmail.com>
2026-08-20 15:58:16 +0530

    Time: 0 ms (100%), Space: 52.5 MB (95.81%) - LeetHub

--- LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        if(n==1){
+        if(n==1){//edge case if there is a single element in array 
             return nums[0];
-        }
+        }//explicitly handle the first and last element in array
         if(nums[0]!=nums[1]) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low=1;
+        int low=1;//initiallize the low from second element and the high from the second last element to properly check left and right 
         int high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
-            }
+            }            //if we are in the left 
             if((mid%2!=0 && nums[mid]==nums[mid-1]) 
             || (mid%2==0&&nums[mid]==nums[mid+1])){
                 low=mid+1;
-            }else{
+            }else{//if we are in the right 
                 high=mid-1;
             }
         }

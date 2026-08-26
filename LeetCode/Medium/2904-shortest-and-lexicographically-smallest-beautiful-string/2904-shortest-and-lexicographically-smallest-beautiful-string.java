e661a214ff03b715d910f381d13002a6029dd0bf
Aditya Raj <adityachoudhary7898@gmail.com>
2026-08-27 02:03:44 +0530

    Time: 3 ms (22.97%), Space: 44.6 MB (10.14%) - LeetHub

---
+++ LeetCode/Medium/2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int low =0;
+        int len = Integer.MAX_VALUE;
+        String ans ="";
+        for(int high=0;high<s.length();high++){
+            char Highch = s.charAt(high);
+            if(Highch == '1'){
+                map.put(Highch , map.getOrDefault(Highch,0)+1);
+            }
+            while(map.getOrDefault('1',0)>k){
+                char Lowch = s.charAt(low);
+                if(map.containsKey(Lowch)){
+                    map.put(Lowch , map.get(Lowch)-1);
+                    if(map.get(Lowch) == 0){
+                        map.remove(Lowch);
+                    }
+                }
+                low++;
+                
+            }
+             if (map.getOrDefault('1', 0) == k) {
+                // Skip those  unnecessary zeroes from left
+                while (s.charAt(low)=='0') {
+                    low++;
+                }//checking the length of substring and comparing with the smallest
+                int curLen = high-low+1;
+                if(curLen <len){
+                    len= curLen;
+                    ans = s.substring(low,high+1);//storing the answer 
+                }else if(curLen==len){//in case of same we use compareTo function
+                    String current = s.substring(low,high+1);
+                    if(current.compareTo(ans)<0){
+                        ans = current;
+                    }
+                }
+             }
+        }
+        
+        return ans;
+    }
+}

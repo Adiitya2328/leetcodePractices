5735959d899a2875e0ac4412101b65ac1cfd5c46
Aditya Raj <adityachoudhary7898@gmail.com>
2026-08-28 21:45:57 +0530

    Time: 26 ms (14.4%), Space: 47.8 MB (58.9%) - LeetHub

--- LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -6,14 +6,16 @@ class Solution {
             totalHrs+=Math.ceil((double)arr[i]/(double)hrs);
         }
         return totalHrs;
+        //simply created a different function to calculate how many total hrs it will take with this array
     }
     public int minEatingSpeed(int[] piles, int h) {
         int max = Integer.MIN_VALUE;
+        //calculate the highest in the array this will decide the range of the BS
         for(int i=0;i<piles.length;i++){
             max = Math.max(max,piles[i]);
         }
         int low=1;
-        int high=max;
+        int high=max;//initialized the range in the BS
         while(low<=high){
             int mid = (low+high)/2;
             int total = calculateTotalHours(piles, mid);

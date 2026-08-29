ef4d0262523668bfc2b5cc3db6e7f70088977f00
Aditya Raj <adityachoudhary7898@gmail.com>
2026-08-29 21:24:59 +0530

    Time: 16 ms (92.09%), Space: 82.6 MB (67.21%) - LeetHub

--- LeetCode/Medium/1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ LeetCode/Medium/1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -9,17 +9,17 @@ class Solution {
                 noOfBQ+=(count/b);
                 count=0;
             }
-            
+           //first of all above created a function to check if it is possible to get those no of bouqeuts 
             
         }
         noOfBQ += (count/b);
         return noOfBQ >= a;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        long val = (long)m*k;
+        long val = (long)m*k;//one edge case if m*k > size of array
         if(val>bloomDay.length){
             return -1;
-        }
+        }//figure out the range of answer will be between max and min of array
         int minm = Integer.MAX_VALUE;
         int maxm = Integer.MIN_VALUE;
         for(int i=0;i<bloomDay.length;i++){
@@ -27,7 +27,7 @@ class Solution {
             maxm = Math.max(maxm,bloomDay[i]);
         }
         int low = minm;
-        int high=maxm;
+        int high=maxm;//applied the BS to find out the perfect answer lying between the range 
         while(low<=high){
             int mid = (low+high)/2;
             if(possible(bloomDay, mid,m ,k)){

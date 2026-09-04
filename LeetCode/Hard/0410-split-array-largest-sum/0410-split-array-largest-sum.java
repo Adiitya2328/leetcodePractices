806549577e94121ac3cbffc7cac0f8b444891e4f
Aditya Raj <adityachoudhary7898@gmail.com>
2026-09-04 23:13:30 +0530

    Time: 0 ms (100%), Space: 42.9 MB (68.81%) - LeetHub

--- LeetCode/Hard/0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ LeetCode/Hard/0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,41 +1,40 @@
-class Solution {
-    public int countStudents(int[] arr,long pages){
-        int students=1;
-            long pageStudent =0;
+class Solution {//can be solved by the same approach of BOOK ALLOCATION
+//The first thing i need to do is to find the count of subarrays
+    public int countSubarrays(int[] arr,long maxSum){
+        int subarrays=1;
+            long currentSum =0;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]+pageStudent<=pages){
-                pageStudent+=arr[i];
+            if(arr[i]+currentSum<=maxSum){
+                currentSum+=arr[i];
 
             }else{
-                students+=1;
-                pageStudent= arr[i];
+                subarrays+=1;
+                currentSum= arr[i];
             }
         }
-        return students;
+        return subarrays;
     }
-    public int findPages(int[] arr, int k) {
-        // code here
-        if(arr.length<k){
+    
+    public int splitArray(int[] nums, int k) {
+        if(nums.length<k){//handle the edge case 
             return -1;
         }
-        long low=0;
+        long low=0;//the range of search will be from the max element in the array(low) to the sum of all elements in the array(high)
         long high=0;
-        for(int i=0;i<arr.length;i++){
-            low = Math.max(low,arr[i]);
-            high += arr[i];
+        for(int i=0;i<nums.length;i++){
+            low=Math.max(low,nums[i]);
+            high+=nums[i];
         }
         while(low<=high){
-            long mid=low+(high-low)/2;
-            int students = countStudents(arr,mid);
-            if(students>k){
+            long mid=(low+high)/2;
+            int subarray = countSubarrays(nums,mid);
+            if(subarray>k){
                 low=mid+1;
             }else{
                 high=mid-1;
             }
         }
         return (int)low;
-    }
-    public int splitArray(int[] nums, int k) {
-        return findPages(nums,k);
+
     }
 }

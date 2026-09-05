c70a02b74e9b05ac8bd9164ab3421b804ca53e5d
Aditya Raj <adityachoudhary7898@gmail.com>
2026-09-05 22:23:22 +0530

    Time: 7 ms (93.44%), Space: 47 MB (53.13%) - LeetHub

--- LeetCode/Medium/2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ LeetCode/Medium/2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -2,18 +2,21 @@ class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
         HashMap<Integer , Integer> map = new HashMap<>();
-        for(int i=0; i<n;i++){
+        for(int i=0; i<n;i++){//map to store the elements 
             map.put(nums[i] ,map.getOrDefault(nums[i],0)+1);
         }
-        int k = map.size();
+        int k = map.size();//get the number of distinct elements just by the size of map
         int low=0;
         int ans = 0;
+        //map to check whether the element is already present or not 
         HashMap<Integer ,Integer> check = new HashMap<>();
+        //simple sliding window approach for the variable size window 
         for(int high =0; high<n;high++){
             check.put(nums[high],check.getOrDefault(nums[high],0)+1);
-
-            while(check.size() == k){
-                ans +=n-high;
+            //add all the elements of high in the check map
+            while(check.size()==k){//whenever the size is equal to k
+                ans +=n-high;//store the answer
+                //remove the characters at low 
                 check.put(nums[low] , check.get(nums[low])-1);
                 if(check.get(nums[low]) == 0){
                     check.remove(nums[low]);

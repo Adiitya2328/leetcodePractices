e086c12aabbed679d561276c69759af3a877e969
Aditya Raj <adityachoudhary7898@gmail.com>
2026-09-13 22:52:49 +0530

    Time: 3 ms (99.93%), Space: 202.6 MB (61.84%) - LeetHub

--- LeetCode/Medium/2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ LeetCode/Medium/2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,27 +10,40 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode temp=head;
-        int count =0;
-        int mid;
-        if(head==null){
-            return head;
-        }
-        if(head.next ==null){
+        if(head==null||head.next==null){
             return null;
         }
-        while(temp!=null){
-            count++;
-            temp =temp.next;
+        ListNode slow=head;
+        ListNode fast =head;
+        fast=head.next.next;
+        
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        
-            mid = (count/2)-1;
-        
-        temp = head;
-        while(mid!=0){
-            temp=temp.next;
-            mid--;
-        }temp.next = temp.next.next;
+        slow.next = slow.next.next;
         return head;
+        // ListNode temp=head;
+        // int count =0;
+        // int mid;
+        // if(head==null){//if there is no element return head or null
+        //     return head;
+        // }
+        // if(head.next ==null){//if there is one element then it will be the middle one
+        //     return null;
+        // }
+        // while(temp!=null){
+        //     count++;//count the numbers of nodes in the list 
+        //     temp =temp.next;
+        // }
+        
+        //     mid = (count/2)-1;//store the position just before the mid
+        
+        // temp = head;
+        // while(mid!=0){
+        //     temp=temp.next;//reach to that position and delete the connection
+        //     mid--;
+        // }temp.next = temp.next.next;
+        // return head; //brute force
     }
 }
